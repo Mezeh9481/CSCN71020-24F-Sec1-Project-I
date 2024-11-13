62764ce0470f0aa22c18cff5729c96b10d38a544
Mezeh9481 <Mezeh9481@conestogac.on.ca>
2024-11-13 13:10:19 -0500

    Initial setup and structure for basic function

---
+++ PolygonChecker/checkTriangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+void checkTriangle() {
+	double sideA, sideB, sideC;
+
+	printf("Enter three side lengths:\n");
+	scanf_s("%1f %1f %1f", sideA, sideB, sideB);
+
+	if ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))
+	{
+
+	}
+	else {
+		printf("Not a triangle\n");
+	}
+}
