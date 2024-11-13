cf7233f006d0f138b8e5be096bd8010d3f88b3f1
Mezeh9481 <Mezeh9481@conestogac.on.ca>
2024-11-13 13:47:26 -0500

    added angle calculating function

--- PolygonChecker/checkTriangle.c
+++ PolygonChecker/checkTriangle.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#define	M_PI		3.141592653589793238462643
 
 #include "checkTriangle.h"
 
@@ -7,10 +8,13 @@
 	double sideA, sideB, sideC;
 
 	printf("Enter three side lengths:\n");
-	scanf_s("%1f %1f %1f", &sideA, &sideB, &sideC);
+	scanf_s("%lf %lf %lf", &sideA, &sideB, &sideC);
 
 	if ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))
 	{
+		double angleA = acos((pow(sideB, 2) + pow(sideC, 2) - pow(sideA, 2)) / (2 * sideB * sideC)) * (180 / M_PI);
+		double angleB = acos((pow(sideA, 2) + pow(sideC, 2) - pow(sideB, 2)) / (2 * sideA * sideC)) * (180 / M_PI);
+		double angleC = 180 - (angleA + angleB);
 
 	}
 	else {
