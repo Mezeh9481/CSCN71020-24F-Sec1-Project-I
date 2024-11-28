90cebd31c9c659a2d8f1d213764444aaa4516a6b
Ridha_Alobied <ralobied8865@conestogac.on.ca>
2024-11-27 21:28:55 -0500

    Final

--- PolygonChecker/RectangleSolver.h
+++ PolygonChecker/RectangleSolver.h
@@ -13,3 +13,4 @@ double distance(Point p1, Point p2);
 bool is_rectangle(Point points[4]);
 double calculate_perimeter(Point points[4]);
 double calculate_area(Point points[4]);
+void clear_input_buffer();
